c689cf52306073738c7bd97bf2aae745d8174576
josevsousa <jose.vicente.de.sousa@gmail.com>
2023-09-18 08:01:13 -0300

    add firebase auth

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,14 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 
 import { routes } from './app.routes';
+import { enviroment } from 'src/environment/environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes) ]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(
+      provideFirebaseApp(()=> initializeApp(enviroment.firebase))
+    ) ]
 };
---
+++ src/environment/environment.ts
@@ -0,0 +1,10 @@
+export const enviroment = {
+    firebase:    {
+        apiKey: "AIzaSyBBaTOVRK4zkYrN5LsXWPy-U55ssy7EBTQ",
+        authDomain: "fireauth-b83b0.firebaseapp.com",
+        projectId: "fireauth-b83b0",
+        storageBucket: "fireauth-b83b0.appspot.com",
+        messagingSenderId: "382633755494",
+        appId: "1:382633755494:web:8d43f5850e7968347c7ea7"
+      }
+}
